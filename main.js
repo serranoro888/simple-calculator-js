7fff078c51a9313af5142b9db65e147d1601289b
Rodolfo serrano <serranoro888@northwested.org>
2024-04-22 13:57:35 -0400

    Updated project script

---
+++ main.js
@@ -0,0 +1,17 @@
+const num1 =parseFloat.getElementById("num1-in").value;
+const num2 =parseFloat.getElementById("num2-in").value;
+
+const myOutput = `
+First number: ${num1}
+Second number: ${num2}
+
+
+Total Number ${totalnumber}
+AVG Number ${averageNumber}`;
+
+console.log(myOutput);
+
+
+
+
+
