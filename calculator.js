aade43c10f7a8cd3783d73734f823fd6a89238f7
Rodolfo serrano <serranoro888@northwested.org>
2024-04-22 13:55:47 -0400

    Simple Js caclulator

---
+++ calculator.js
@@ -0,0 +1,46 @@
+
+// Comment block
+
+
+
+
+// Input
+
+const nombre = 'Rudy Serrano';
+const date = '22 APR 2024';
+const project = 'JS Simple Calculator';
+
+
+
+const num1 = 10;
+const num2 = 4;
+
+
+// Process
+// JS does the math for us
+// Write some equations that tell JS how to do the math
+const output = 'Output:'
+// Addition
+const addition = num1 + num2 ;
+// Subtraction
+const subtraction = num1 - num2 ;
+// Multiplication
+const multiplication = num1 * num2 ;
+// Division
+const division = num1 / num2 ;
+
+
+//Output
+//Show answers or the results from the script
+const myOutput = `
+    ${nombre}
+    ${date}
+    ${project}
+    ${output}
+   10 + 4 = ${addition}
+   10 - 4 = ${subtraction}
+   10 * 4 = ${multiplication}
+   10 / 4 = ${division}
+`
+console.log(myOutput)
+
